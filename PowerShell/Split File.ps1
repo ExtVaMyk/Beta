51c0ac3a2e9ef9ecaaf98f151d72f888f5ea5445
ReportsForNAV <mb@fornav.com>
2017-03-24 18:50:31 +0100

    Initial Beta Version
    Initial Reports.

---
+++ PowerShell/Split File.ps1
@@ -0,0 +1,6 @@
+﻿Set-ExecutionPolicy unrestricted
+
+import-module "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\NavModelTools.ps1"
+import-module "C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1"
+
+Split-NAVApplicationObjectFile -Source "Q:\ForNAV\Add-On\Objects.txt" -Destination "Q:\ForNAV\Add-On\Objects\" -Force
